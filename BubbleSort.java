a2553a6b95f63a53441b8b7f4cbec8009e039620
Ayush29801 <122856040+Ayush29801@users.noreply.github.com>
2023-01-18 11:56:43 +0400

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+/*
+ *  Bubble sort is a simple sorting algorithm that repeatedly steps through the list to be sorted
+ *  has a time complexity of O(n^2) in the worst and average cases 
+ *  O(n) in the best case
+ *  
+ */
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
